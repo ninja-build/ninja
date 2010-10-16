4a58adb172b472765ef26e8f8e22acd7bc90ff95
Evan Martin <martine@danga.com>
2010-10-16 11:00:51 -0700

    rearrange

--- ninja_test.cc
+++ ninja_test.cc
@@ -2,61 +2,6 @@
 
 #include <gtest/gtest.h>
 
-struct NinjaTest : public testing::Test {
-  NinjaTest() {
-    rule_cat_ = state_.AddRule("cat", "cat @in > $out");
-  }
-
-  Rule* rule_cat_;
-  State state_;
-};
-
-TEST_F(NinjaTest, Basic) {
-  Edge* edge = state_.AddEdge(rule_cat_);
-  state_.AddInOut(edge, Edge::IN, "in1");
-  state_.AddInOut(edge, Edge::IN, "in2");
-  state_.AddInOut(edge, Edge::OUT, "out");
-
-  EXPECT_EQ("cat in1 in2 > out", edge->EvaluateCommand());
-
-  EXPECT_FALSE(state_.GetNode("in1")->dirty());
-  EXPECT_FALSE(state_.GetNode("in2")->dirty());
-  EXPECT_FALSE(state_.GetNode("out")->dirty());
-
-  state_.stat_cache()->GetFile("in1")->Touch(1);
-  EXPECT_TRUE(state_.GetNode("in1")->dirty());
-  EXPECT_FALSE(state_.GetNode("in2")->dirty());
-  EXPECT_TRUE(state_.GetNode("out")->dirty());
-
-  Plan plan(&state_);
-  plan.AddTarget("out");
-  edge = plan.FindWork();
-  ASSERT_TRUE(edge);
-  EXPECT_EQ("cat in1 in2 > out", edge->EvaluateCommand());
-  ASSERT_FALSE(plan.FindWork());
-}
-
-struct TestEnv : public EvalString::Env {
-  virtual string Evaluate(const string& var) {
-    return vars[var];
-  }
-  map<string, string> vars;
-};
-TEST(EvalString, PlainText) {
-  EvalString str;
-  str.Parse("plain text");
-  ASSERT_EQ("plain text", str.Evaluate(NULL));
-}
-TEST(EvalString, OneVariable) {
-  EvalString str;
-  ASSERT_TRUE(str.Parse("hi $var"));
-  EXPECT_EQ("hi $var", str.unparsed());
-  TestEnv env;
-  EXPECT_EQ("hi ", str.Evaluate(&env));
-  env.vars["$var"] = "there";
-  EXPECT_EQ("hi there", str.Evaluate(&env));
-}
-
 TEST(Parser, Empty) {
   State state;
   ManifestParser parser(&state);
@@ -84,4 +29,52 @@ TEST(Parser, Rules) {
   Rule* rule = state.rules_.begin()->second;
   EXPECT_EQ("cat", rule->name_);
   EXPECT_EQ("cat @in > $out", rule->command_.unparsed());
+}
+
+TEST(State, Basic) {
+  State state;
+  Rule* rule = state.AddRule("cat", "cat @in > $out");
+  Edge* edge = state.AddEdge(rule);
+  state.AddInOut(edge, Edge::IN, "in1");
+  state.AddInOut(edge, Edge::IN, "in2");
+  state.AddInOut(edge, Edge::OUT, "out");
+
+  EXPECT_EQ("cat in1 in2 > out", edge->EvaluateCommand());
+
+  EXPECT_FALSE(state.GetNode("in1")->dirty());
+  EXPECT_FALSE(state.GetNode("in2")->dirty());
+  EXPECT_FALSE(state.GetNode("out")->dirty());
+
+  state.stat_cache()->GetFile("in1")->Touch(1);
+  EXPECT_TRUE(state.GetNode("in1")->dirty());
+  EXPECT_FALSE(state.GetNode("in2")->dirty());
+  EXPECT_TRUE(state.GetNode("out")->dirty());
+
+  Plan plan(&state);
+  plan.AddTarget("out");
+  edge = plan.FindWork();
+  ASSERT_TRUE(edge);
+  EXPECT_EQ("cat in1 in2 > out", edge->EvaluateCommand());
+  ASSERT_FALSE(plan.FindWork());
+}
+
+struct TestEnv : public EvalString::Env {
+  virtual string Evaluate(const string& var) {
+    return vars[var];
+  }
+  map<string, string> vars;
+};
+TEST(EvalString, PlainText) {
+  EvalString str;
+  str.Parse("plain text");
+  ASSERT_EQ("plain text", str.Evaluate(NULL));
+}
+TEST(EvalString, OneVariable) {
+  EvalString str;
+  ASSERT_TRUE(str.Parse("hi $var"));
+  EXPECT_EQ("hi $var", str.unparsed());
+  TestEnv env;
+  EXPECT_EQ("hi ", str.Evaluate(&env));
+  env.vars["$var"] = "there";
+  EXPECT_EQ("hi there", str.Evaluate(&env));
 }
