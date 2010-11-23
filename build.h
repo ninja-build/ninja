b7b79dde787612aeff7f65dc9f84d53577ac885e
Evan Martin <martine@danga.com>
2010-11-22 17:08:27 -0800

    add docs to Plan

--- build.h
+++ build.h
@@ -7,15 +7,27 @@ struct DiskInterface;
 struct Node;
 struct State;
 
+// Plan stores the state of a build plan: what we intend to build,
+// which steps we're ready to execute.
 struct Plan {
+  // Add a target to our plan (including all its dependencies).
+  // Returns false if we don't need to build this target; may
+  // fill in |err| with an error message if there's a problem.
   bool AddTarget(Node* node, string* err);
 
+  // Pop a ready edge off the queue of edges to build.
+  // Returns NULL if there's no work to do.
   Edge* FindWork();
-  void EdgeFinished(Edge* edge);
 
+  // Returns true if there's more work to be done.
   bool more_to_do() const { return !want_.empty(); }
 
+  // Dumps the current state of the plan.
   void Dump();
+
+  // Mark an edge as done building.  Used internally and by
+  // tests.
+  void EdgeFinished(Edge* edge);
 
 private:
   void NodeFinished(Node* node);
