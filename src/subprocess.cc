13977a2d78ca713556d9f6b47c196beb827502f2
Evan Martin <martine@danga.com>
2011-01-08 15:51:05 -0800

    work around an unused variable warning

--- src/subprocess.cc
+++ src/subprocess.cc
@@ -71,7 +71,8 @@ bool Subprocess::Start(const string& command) {
     // If we get here, something went wrong; the execl should have
     // replaced us.
     char* err = strerror(errno);
-    write(error_pipe, err, strlen(err));
+    int unused = write(error_pipe, err, strlen(err));
+    unused = unused;  // If the write fails, there's nothing we can do.
     _exit(1);
   }
 
