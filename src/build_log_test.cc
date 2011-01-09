667f48b5ef5cdda883488cf5e07d75744d76c919
Evan Martin <martine@danga.com>
2011-01-09 10:23:21 -0800

    remove tempnam

--- src/build_log_test.cc
+++ src/build_log_test.cc
@@ -2,15 +2,14 @@
 
 #include "test.h"
 
+static const char kTestFilename[] = "BuildLogTest-tempfile";
+
 struct BuildLogTest : public StateTestWithBuiltinRules {
   virtual void SetUp() {
-    log_filename_ = tempnam(NULL, "ninja");
   }
   virtual void TearDown() {
-    unlink(log_filename_.c_str());
+    unlink(kTestFilename);
   }
-
-  string log_filename_;
 };
 
 TEST_F(BuildLogTest, WriteRead) {
@@ -20,14 +19,14 @@ TEST_F(BuildLogTest, WriteRead) {
 
   BuildLog log1;
   string err;
-  EXPECT_TRUE(log1.OpenForWrite(log_filename_, &err));
+  EXPECT_TRUE(log1.OpenForWrite(kTestFilename, &err));
   ASSERT_EQ("", err);
   log1.RecordCommand(state_.edges_[0], 15);
   log1.RecordCommand(state_.edges_[1], 20);
   log1.Close();
 
   BuildLog log2;
-  EXPECT_TRUE(log2.Load(log_filename_, &err));
+  EXPECT_TRUE(log2.Load(kTestFilename, &err));
   ASSERT_EQ("", err);
 
   ASSERT_EQ(2, log1.log_.size());
@@ -42,14 +41,14 @@ TEST_F(BuildLogTest, WriteRead) {
 }
 
 TEST_F(BuildLogTest, DoubleEntry) {
-  FILE* f = fopen(log_filename_.c_str(), "wb");
+  FILE* f = fopen(kTestFilename, "wb");
   fprintf(f, "0 out command abc\n");
   fprintf(f, "0 out command def\n");
   fclose(f);
 
   string err;
   BuildLog log;
-  EXPECT_TRUE(log.Load(log_filename_, &err));
+  EXPECT_TRUE(log.Load(kTestFilename, &err));
   ASSERT_EQ("", err);
 
   BuildLog::LogEntry* e = log.LookupByOutput("out");
