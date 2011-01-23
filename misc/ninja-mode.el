c1cb4f5ee82b1d41a5fb028aea0c6daa0461f050
Evan Martin <martine@danga.com>
2011-01-22 19:05:26 -0800

    fancier elisp, handle "include"

--- misc/ninja-mode.el
+++ misc/ninja-mode.el
@@ -2,10 +2,13 @@
 ;; Just some syntax highlighting for now.
 
 (setq ninja-keywords
-      '(("^#.*" . font-lock-comment-face)
-        ("^\\<rule\\>\\|^\\<build\\>\\|^\\<subninja\\>" . font-lock-keyword-face)
-        ("\\([[:alnum:]_]+\\) =" . (1 font-lock-variable-name-face))
-        ))
+      (list
+       '("^#.*" . font-lock-comment-face)
+       (cons (concat "^" (regexp-opt '("rule" "build" "subninja" "include")
+                                     'words))
+             font-lock-keyword-face)
+       '("\\([[:alnum:]_]+\\) =" . (1 font-lock-variable-name-face))
+       ))
 (define-derived-mode ninja-mode fundamental-mode "ninja"
   (setq comment-start "#")
   ; Pass extra "t" to turn off syntax-based fontification -- we don't want
