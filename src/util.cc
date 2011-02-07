d08ee9101f03bbdd5eeb85f9b944a2919257a213
Stefan `Sec` Zehl <sec@42.org>
2011-02-07 17:01:38 +0100

    Make it compile on FreeBSD

--- src/util.cc
+++ src/util.cc
@@ -14,17 +14,9 @@
 
 #include "util.h"
 
-#include <execinfo.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-void DumpBacktrace(int skip_frames) {
-  void* stack[256];
-  int size = backtrace(stack, 256);
-  ++skip_frames;  // Skip ourselves as well.
-  backtrace_symbols_fd(stack + skip_frames, size - skip_frames, 2);
-}
 
 void Fatal(const char* msg, ...) {
   va_list ap;
@@ -33,6 +25,5 @@ void Fatal(const char* msg, ...) {
   vfprintf(stderr, msg, ap);
   va_end(ap);
   fprintf(stderr, "\n");
-  DumpBacktrace(1);
   exit(1);
 }
