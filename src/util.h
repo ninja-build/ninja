ece2d63323bde9fd89956193167e09708802d73c
Thiago Farina <tfransosi@gmail.com>
2011-04-15 12:22:07 -0700

    Remove the declaration of DumpBacktrace.
    This function was removed on 01880fb3a2a13f9071e9729c3a13752846828ed2.

--- src/util.h
+++ src/util.h
@@ -18,11 +18,6 @@
 
 #include <string>
 
-// Dump a backtrace to stderr.
-// |skip_frames| is how many frames to skip;
-// DumpBacktrace implicitly skips itself already.
-void DumpBacktrace(int skip_frames);
-
 // Log a fatal message, dump a backtrace, and exit.
 void Fatal(const char* msg, ...);
 
