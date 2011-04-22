df8ef0f220f44709d083c7018f96dd4a5f3687b8
Firstname Lastname <philip@pobox.com>
2011-04-22 22:28:32 +0100

    Built ninja-bootstrap into a temporary directory. Support the manifest tool, to let Windows binaries find the MSVCRT runtime.

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,1 +1,3 @@
-cl.exe /nologo /EHsc /I. /Isrc /DWIN32 /Feninja-bootstrap.exe src\build.cc src\build_log.cc src\eval_env.cc src\graph.cc src\graphviz.cc src\parsers.cc src\subprocess.win32.cpp src\util.cc src\ninja_jumble.cc src\ninja.cc src\getopt.c shlwapi.lib && .\ninja-bootstrap.exe -f build-win32.ninja ninja.exe && del .\ninja-bootstrap.exe
+# rmdir /q /s Bootstrap && mkdir Bootstrap
+
+cl.exe /nologo /EHsc /I. /Isrc /DWIN32 /DNDEBUG /D_CONSOLE /MD /Fo"Bootstrap\\" /W3 /Feninja-bootstrap.exe src\build.cc src\build_log.cc src\eval_env.cc src\graph.cc src\graphviz.cc src\parsers.cc src\subprocess.win32.cpp src\util.cc src\ninja_jumble.cc src\ninja.cc src\getopt.c /link /incremental:no /subsystem:Console /opt:ref /opt:icf shlwapi.lib && .\ninja-bootstrap.exe -f build-win32.ninja ninja.exe && del .\ninja-bootstrap.exe
