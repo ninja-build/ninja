86360aa50d570e774dd6143b9ac3f3d67eb14422
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-05-08 00:56:26 +0900

    fix to compiling src/subprocess-win32.cc on bootstraping

--- src/subprocess-win32.cc
+++ src/subprocess-win32.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef _WIN32
+
 #include "subprocess.h"
 
 #include <stdio.h>
@@ -209,3 +211,5 @@ Subprocess* SubprocessSet::NextFinished() {
   finished_.pop();
   return subproc;
 }
+
+#endif // _WIN32
