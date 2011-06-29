e5211705799738d05ce1ff6f1bc125866878fe5e
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-06-29 20:03:12 +0900

    * src/getopt.c: add "const".

--- src/getopt.c
+++ src/getopt.c
@@ -143,7 +143,7 @@ is_option (char *argv_element, int only)
 
 /* getopt_internal:  the function that does all the dirty work */
 static int
-getopt_internal (int argc, char **argv, char *shortopts,
+getopt_internal (int argc, char **argv, const char *shortopts,
                  GETOPT_LONG_OPTION_T * longopts, int *longind, int only)
 {
   GETOPT_ORDERING_T ordering = PERMUTE;
@@ -155,7 +155,7 @@ getopt_internal (int argc, char **argv, char *shortopts,
   char *possible_arg = NULL;
   int longopt_match = -1;
   int has_arg = -1;
-  char *cp;
+  const char *cp;
   int arg_next = 0;
 
   /* first, deal with silly parameters and easy stuff */
@@ -392,14 +392,14 @@ getopt (int argc, char **argv, char *optstring)
 }
 
 int
-getopt_long (int argc, char **argv, char *shortopts,
+getopt_long (int argc, char **argv, const char *shortopts,
              GETOPT_LONG_OPTION_T * longopts, int *longind)
 {
   return getopt_internal (argc, argv, shortopts, longopts, longind, 0);
 }
 
 int
-getopt_long_only (int argc, char **argv, char *shortopts,
+getopt_long_only (int argc, char **argv, const char *shortopts,
                   GETOPT_LONG_OPTION_T * longopts, int *longind)
 {
   return getopt_internal (argc, argv, shortopts, longopts, longind, 1);
