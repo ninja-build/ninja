9e2f793ee4e343579ceba3fb2dc1f90b75029019
Scott Graham <scott.github@h4ck3r.net>
2011-07-30 17:51:38 -0700

    don't use grep in bootstrap

--- bootstrap.bat
+++ bootstrap.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 echo Building ninja manually...
 
-FOR /F "usebackq tokens=*" %%i in (`dir /s/b src\*.cc src\*.c ^| grep -v test ^| grep -v subprocess.cc ^| grep -v browse.cc`) do set srcs=!srcs! %%i
+FOR /F "usebackq tokens=*" %%i in (`dir /s/b src\*.cc src\*.c ^| findstr /v test ^| findstr /v subprocess.cc ^| findstr /v browse.cc`) do set srcs=!srcs! %%i
 
 cl /nologo /Zi /W4 /WX /D_CRT_SECURE_NO_WARNINGS /wd4530 /wd4512 /wd4706 /wd4100 %SRCS% /link /out:ninja.bootstrap.exe
 if %ERRORLEVEL% gtr 0 goto dead
