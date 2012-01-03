d7dbe1ac84658bd2b1b1d5cf38823a22a317f18b
Frances Buontempo <frances.buontempo@gmail.com>
2012-01-03 16:45:16 +0000

    typedef long long for MSVC

---
+++ src/win32port.h
@@ -0,0 +1,9 @@
+#ifndef NINJA_WIN32PORT_H_
+#define NINJA_WIN32PORT_H_
+#pragma once
+
+/// A 64-bit integer type
+typedef unsigned long long int64_t;
+typedef unsigned long long uint64_t;
+
+#endif // NINJA_WIN32PORT_H_
