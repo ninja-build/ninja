ada42e5eee8d35c359baa828c9403fa406c1db8b
Nico Weber <thakis@chromium.org>
2012-04-30 08:40:12 -0700

    Build without RTTI on windows (/GR-), saves 1kB and is consisten with linux/mac.

--- configure.py
+++ configure.py
@@ -103,7 +103,7 @@ else:
 
 if platform == 'windows':
     cflags = ['/nologo', '/Zi', '/W4', '/WX', '/wd4530', '/wd4100', '/wd4706',
-              '/wd4512', '/wd4800', '/wd4702', '/wd4819',
+              '/wd4512', '/wd4800', '/wd4702', '/wd4819', '/GR-',
               '/DNOMINMAX', '/D_CRT_SECURE_NO_WARNINGS',
               "/DNINJA_PYTHON=\"%s\"" % (options.with_python,)]
     ldflags = ['/DEBUG', '/libpath:$builddir']
