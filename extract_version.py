43fef5aa216282057fe5950e6e2445378fb89135
Scott Graham <scottmg@chromium.org>
2012-06-13 16:55:26 -0700

    remove unneeded stuff

--- extract_version.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-p = subprocess.Popen(['git', 'describe', '--always'], shell=True,
-                     stdout=subprocess.PIPE)
-version = p.communicate()[0].strip()
-f = open('src/version.h', 'w')
-print >>f, 'const char kVersionString[] = "%s";' % version
-f.close()
