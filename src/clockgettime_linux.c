74d1277621b8dd204c8f677eef6dd6522576b721
Claus Klein <claus.klein@arcormail.de>
2012-06-20 22:44:19 +0200

    quickfix for missing clockgettime under linux
    (use direct syscall instead)

---
+++ src/clockgettime_linux.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <errors.h>
+
+#if defined(_POSIX_TIMERS) && _POSIX_TIMERS > 0
+#ifdef __linux__
+#include <sys/syscall.h>
+/* libc has incredibly messy way of doing this,
+ * typically requiring -lrt. We just skip all this mess */
+int clock_gettime(clockid_t clock_id, struct timespec *ts) {
+    if(syscall(__NR_clock_gettime, clock_id, ts)) {
+        return errno;
+    } else {
+        return 0;
+    }
+}
+#endif
+#endif
+
