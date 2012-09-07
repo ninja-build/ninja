df079fcbcded45241d65abc2de2d1b168f36cd22
Alex Caudill <alex.caudill@gmail.com>
2012-09-07 16:49:15 +0000

    add GetProcessorCount() implementation for Solaris

--- src/util.cc
+++ src/util.cc
@@ -37,6 +37,7 @@
 #if defined(__APPLE__) || defined(__FreeBSD__)
 #include <sys/sysctl.h>
 #elif defined(__SVR4) && defined(__sun) 
+#include <sys/sysconf.h>
 #include <sys/loadavg.h>
 #elif defined(linux)
 #include <sys/sysinfo.h>
@@ -309,6 +310,10 @@ int GetProcessorCount() {
     return 0;
   }
   return processors;
+}
+#elif defined(__SVR4) && defined(__sun) 
+int GetProcessorCount() {
+  return sysconf(_SC_NPROCESSORS_ONLN);
 }
 #elif defined(_WIN32)
 int GetProcessorCount() {
