7c3b8455ef0243f9c00c3d82d2ce74d79909365e
Jon <jon.forums@gmail.com>
2012-09-20 10:08:30 -0400

    Silence bothersome warning from -Wextra
    Struct initializations such as those in `CLWrapper::Run` of file
    `src/msvc_helper-win32.cc` causes MinGW GCC to spew warnings.

--- configure.py
+++ configure.py
@@ -140,6 +140,7 @@ else:
               '-fno-rtti',
               '-fno-exceptions',
               '-fvisibility=hidden', '-pipe',
+              '-Wno-missing-field-initializers',
               '-DNINJA_PYTHON="%s"' % options.with_python]
     if options.debug:
         cflags += ['-D_GLIBCXX_DEBUG', '-D_GLIBCXX_DEBUG_PEDANTIC']
