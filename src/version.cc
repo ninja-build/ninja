9f1852fa3c97197e1876f1d47ca45e66b5e6cd28
Evan Martin <martine@danga.com>
2013-04-08 14:44:30 -0700

    sync version number in master with release
    Update RELEASING with notes on how to not screw this up again.

--- src/version.cc
+++ src/version.cc
@@ -18,7 +18,7 @@
 
 #include "util.h"
 
-const char* kNinjaVersion = "1.1.0.git";
+const char* kNinjaVersion = "1.2.0.git";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
