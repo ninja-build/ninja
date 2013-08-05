ba71bc32312800fa1bc897d6b8c0280bee488acd
Kim Grasman <kim.grasman@gmail.com>
2013-08-05 17:52:51 +0200

    Fix tabs/spaces inconsistency.

--- platform_helper.py
+++ platform_helper.py
@@ -41,9 +41,8 @@ class Platform( object ):
             self._platform = 'mingw'
         elif self._platform.startswith('win'):
             self._platform = 'msvc'
-	elif self._platform.startswith('bitrig'):
-	    self._platform = 'bitrig'
-
+        elif self._platform.startswith('bitrig'):
+            self._platform = 'bitrig'
 
     def platform(self):
         return self._platform
