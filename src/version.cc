6f7ea464bb9161ce2e15deb97977886de152c12d
Evan Martin <martine@danga.com>
2013-09-11 19:05:21 -0700

    mark this 1.4.0.git, update RELEASING

--- src/version.cc
+++ src/version.cc
@@ -18,7 +18,7 @@
 
 #include "util.h"
 
-const char* kNinjaVersion = "1.3.0.git";
+const char* kNinjaVersion = "1.4.0.git";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
