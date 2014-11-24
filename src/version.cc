b532cab080bbde2068ab49aba814c7176111681f
Nico Weber <thakis@chromium.org>
2014-11-24 09:33:39 -0800

    mark this 1.5.3.git

--- src/version.cc
+++ src/version.cc
@@ -18,7 +18,7 @@
 
 #include "util.h"
 
-const char* kNinjaVersion = "1.5.1.git";
+const char* kNinjaVersion = "1.5.3.git";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
