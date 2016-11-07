b0cce09f563d0942fcf1c1256db679cebcbd6bea
Nico Weber <nicolasweber@gmx.de>
2016-11-07 10:28:51 -0800

    mark this 1.7.2.git

--- src/version.cc
+++ src/version.cc
@@ -18,7 +18,7 @@
 
 #include "util.h"
 
-const char* kNinjaVersion = "1.7.1.git";
+const char* kNinjaVersion = "1.7.2.git";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
