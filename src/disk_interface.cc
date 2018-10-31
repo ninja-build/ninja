7921b6c333b4ac3d73e78573377578bfafa932a0
kifof <36780019+kifof@users.noreply.github.com>
2018-10-31 15:39:03 -0700

    Fixing disk_interface_test failure on windows (see #1420).

--- src/disk_interface.cc
+++ src/disk_interface.cc
@@ -173,6 +173,8 @@ TimeStamp RealDiskInterface::Stat(const string& path, string* err) const {
     // StatAllFilesInDir does not report any information for base = "..".
     base = ".";
     dir = path;
+  } else if (!dir.size()) {
+    dir = ".";
   }
 
   transform(dir.begin(), dir.end(), dir.begin(), ::tolower);
