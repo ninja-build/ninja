66b05496ff0ef6114e3534418c970dd92d34accb
David 'Digit' Turner <digit+github@google.com>
2022-02-18 15:05:10 +0100

    Ensure the `msvc` tool is built in all Win32 Ninja binaries.
    It was only previously available when Ninja was built when
    `_MSVC` is defined (i.e. when compiling with the Microsoft
    compiler of with `clang-cl`).
    
    + Tag the tool as DEPRECATED

--- src/ninja.cc
+++ src/ninja.cc
@@ -52,7 +52,7 @@
 
 using namespace std;
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 // Defined in msvc_helper_main-win32.cc.
 int MSVCHelperMain(int argc, char** argv);
 
@@ -449,7 +449,7 @@ int NinjaMain::ToolBrowse(const Options*, int, char**) {
 }
 #endif
 
-#if defined(_MSC_VER)
+#if defined(_WIN32)
 int NinjaMain::ToolMSVC(const Options* options, int argc, char* argv[]) {
   // Reset getopt: push one argument onto the front of argv, reset optind.
   argc++;
@@ -1043,8 +1043,8 @@ const Tool* ChooseTool(const string& tool_name) {
   static const Tool kTools[] = {
     { "browse", "browse dependency graph in a web browser",
       Tool::RUN_AFTER_LOAD, &NinjaMain::ToolBrowse },
-#if defined(_MSC_VER)
-    { "msvc", "build helper for MSVC cl.exe (EXPERIMENTAL)",
+#ifdef _WIN32
+    { "msvc", "build helper for MSVC cl.exe (DEPRECATED)",
       Tool::RUN_AFTER_FLAGS, &NinjaMain::ToolMSVC },
 #endif
     { "clean", "clean built files",
