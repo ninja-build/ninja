ff06e229878a780192aee3c6d5d9d270e8b4c4b0
Jan Niklas Hasse <jhasse@bixense.com>
2022-08-30 21:43:53 +0200

    mark this 1.12.0.git

--- src/version.cc
+++ src/version.cc
@@ -20,7 +20,7 @@
 
 using namespace std;
 
-const char* kNinjaVersion = "1.11.0.git";
+const char* kNinjaVersion = "1.12.0.git";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
