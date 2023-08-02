c27016fd451e3a1dc6f39e288d46089d998b8673
Ed Catmur <edward.catmur@mavensecurities.com>
2023-08-02 15:29:14 +0100

    Update status on edge finished
    In the terminal, when an edge finishes update the status to display the
    first started (longest running) action.
    
    Usually this will be immediately overwritten with the next started
    action but if there is a bottleneck in the build or if it is reaching
    the end, this ensures that the console displays status of a build that
    is actually running and not one that has completed.
    
    This ensures that the user attributes any delay to the (or an) action
    that is actually causing the slowdown and not unfairly to an action
    that has already finished just because it happened to be the last
    started.
    
    To make this clear, also change the default NINJA_STATUS to include
    current actual parallelism level - this will reduce from number of cores
    to 1 as the bottleneck or end of build approaches.
    
    Implementation stores currently running nodes in a deque, with null for
    interior completed nodes. This should be fairly efficient and allows
    future extension to e.g. display multiple parallel action names, as in
    
    Partially resolves #111 (should be enough for most purposes).

--- src/status_printer.cc
+++ src/status_printer.cc
@@ -76,8 +76,12 @@ StatusPrinter::StatusPrinter(const BuildConfig& config)
     progress_status_format_ = NULL;
   } else {
     progress_status_format_ = getenv("NINJA_STATUS");
-    if (!progress_status_format_)
-      progress_status_format_ = "[%f/%t] ";
+    if (!progress_status_format_) {
+      if (printer_.is_smart_terminal())
+        progress_status_format_ = "[%f*%r/%t] ";
+      else
+        progress_status_format_ = "[%f/%t] ";
+    }
   }
 }
 
@@ -114,6 +118,8 @@ void StatusPrinter::BuildEdgeStarted(const Edge* edge,
   ++started_edges_;
   ++running_edges_;
   time_millis_ = start_time_millis;
+  if (printer_.is_smart_terminal())
+    edges_.push_back(edge);
 
   if (edge->use_console() || printer_.is_smart_terminal())
     PrintStatus(edge, start_time_millis);
@@ -218,6 +224,12 @@ void StatusPrinter::BuildEdgeFinished(Edge* edge, int64_t start_time_millis,
   } else
     --eta_unpredictable_edges_remaining_;
 
+  if (printer_.is_smart_terminal()) {
+    *std::find(edges_.begin(), edges_.end(), edge) = nullptr;
+    while (!edges_.empty() && edges_.front() == nullptr)
+      edges_.pop_front();
+  }
+
   if (edge->use_console())
     printer_.SetConsoleLocked(false);
 
@@ -275,6 +287,9 @@ void StatusPrinter::BuildEdgeFinished(Edge* edge, int64_t start_time_millis,
     _setmode(_fileno(stdout), _O_TEXT);  // End Windows extra CR fix
 #endif
   }
+
+  if (printer_.is_smart_terminal() && !edges_.empty())
+    PrintStatus(edges_.front(), end_time_millis);
 }
 
 void StatusPrinter::BuildStarted() {
