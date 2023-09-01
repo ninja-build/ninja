22b538503b50ad23a6caddb966c6816b3db3d89f
Michael Glenn <lostleo86@gmail.com>
2023-08-31 19:31:21 -0500

    Create generated-build-config.js

---
+++ .github/workflows/generated-build-config.js
@@ -0,0 +1,8 @@
+`".$_-0/config-generated-build-config.js @ninja-build.org/build.ninja.html
+# What's new
+ - The last Ninja release is 
+ (v1.11.1), 
+  - released 30 Aug 2022. Read the release notes. 
+  ".$_-0/git-clone-git--github.com/ninja-build/ninja.git "&&" cd_ninja"
+".$_-0/git-checkout_release"
+".$_-0/cat README.md"`
