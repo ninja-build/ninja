e0b2f77b1b7abcb47195aa8d7a8466a53c5dd9c3
David 'Digit' Turner <digit+github@google.com>
2024-04-02 18:21:47 +0200

    util.c: ReadFile(): support Win32 long paths.
    Use CreateFileW() to ensure proper long path support on Win32.

--- src/util.cc
+++ src/util.cc
@@ -416,8 +416,10 @@ int ReadFile(const string& path, string* contents, string* err) {
   // This makes a ninja run on a set of 1500 manifest files about 4% faster
   // than using the generic fopen code below.
   err->clear();
-  HANDLE f = ::CreateFileA(path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
-                           OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+  std::wstring native_path = UTF8ToWin32Unicode(path);
+  HANDLE f =
+      ::CreateFileW(native_path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
+                    OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
   if (f == INVALID_HANDLE_VALUE) {
     err->assign(GetLastErrorString());
     return -ENOENT;
