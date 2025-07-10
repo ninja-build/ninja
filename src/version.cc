79feac0f3e3bc9da9effc586cd5fea41e7550051
Jan Niklas Hasse <jhasse@bixense.com>
2025-07-10 22:43:08 +0200

    v1.13.1

--- src/version.cc
+++ src/version.cc
@@ -20,7 +20,7 @@
 
 using namespace std;
 
-const char* kNinjaVersion = "1.13.0";
+const char* kNinjaVersion = "1.13.1";
 
 void ParseVersion(const string& version, int* major, int* minor) {
   size_t end = version.find('.');
