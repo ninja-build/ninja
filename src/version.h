68fbe8ae5d999e1f007401bcd19aeb234c966103
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2025-09-26 15:24:48 +0200

    Fix formatting in version.h

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@ void ParseVersion(const std::string& version, int* major, int* minor);
 
 /// Check whether \a version is compatible with the current Ninja version,
 /// aborting if not.
-void CheckNinjaVersion(const std::string& required_version, int *file_major,
-    int *file_minor);
+void CheckNinjaVersion(const std::string& required_version, int* file_major,
+                       int* file_minor);
 
 #endif  // NINJA_VERSION_H_
