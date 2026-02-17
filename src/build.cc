d21c6aa114d2d3ee5515ec537a6333e46b274d5a
Nick Powell <powell.ngl@gmail.com>
2026-02-17 11:25:31 +0100

    Update outdated comment on EdgeWeightHeuristic()

--- src/build.cc
+++ src/build.cc
@@ -466,7 +466,8 @@ void Plan::UnmarkDependents(const Node* node, set<Node*>* dependents) {
 namespace {
 
 // Heuristic for edge priority weighting.
-// Phony edges are free (0 cost), all other edges are weighted equally.
+// Phony edges are free (0 cost), all other edges are weighted by their 
+// previous elapsed time if available
 int64_t EdgeWeightHeuristic(Edge *edge) {
   int64_t prev_elapsed_time_millis =
     edge->prev_elapsed_time_millis < 0 ? 1 : edge->prev_elapsed_time_millis;
