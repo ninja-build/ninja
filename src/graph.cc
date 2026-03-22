36c1fd98332935b08b7e7e57b9ac8ac66055c7b5
Brad King <brad.king@kitware.com>
2026-03-22 11:34:28 -0400

    Visit all of an edge's inputs before checking if any is ready
    Newly encountered edges may load dyndep files and gain outputs that
    correspond to some of our inputs.  In that case those inputs' readiness
    is determined by their edges instead of just mtimes.
    
    Fixes: #2641

--- src/graph.cc
+++ src/graph.cc
@@ -168,14 +168,18 @@ bool DependencyScan::RecomputeNodeDirty(Node* node, std::vector<Node*>* stack,
   validation_nodes->insert(validation_nodes->end(),
       edge->validations_.begin(), edge->validations_.end());
 
-  // Visit all inputs; we're dirty if any of the inputs are dirty.
+  // Visit all inputs before checking if any of them is ready.
+  // Newly encountered edges may load dyndep files and gain
+  // outputs that correspond to some of our inputs.
+  for (Node* i : edge->inputs_) {
+    if (!RecomputeNodeDirty(i, stack, validation_nodes, err))
+      return false;
+  }
+
+  // We're dirty if any of the inputs is dirty.
   Node* most_recent_input = NULL;
   for (vector<Node*>::iterator i = edge->inputs_.begin();
        i != edge->inputs_.end(); ++i) {
-    // Visit this input.
-    if (!RecomputeNodeDirty(*i, stack, validation_nodes, err))
-      return false;
-
     // If an input is not ready, neither are our outputs.
     if (Edge* in_edge = (*i)->in_edge()) {
       if (!in_edge->outputs_ready_)
